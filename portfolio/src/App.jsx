e2ba192196e46be8221ca9bd1d5a3e425934d735
dagalera <dagalera.dev@gmail.com>
2024-05-01 11:07:30 -0300

    adicionando tailwind

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <p className=' text-8xl'>
+        Hello World
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
