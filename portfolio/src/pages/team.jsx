84bc0354ec678601116c62a25017c218066bb1f3
dagalera <dagalera.dev@gmail.com>
2024-05-02 12:14:00 -0300

    add linkedin

--- portfolio/src/pages/team.jsx
+++ portfolio/src/pages/team.jsx
@@ -1,7 +1,7 @@
 const people = [
     {
       name: 'Rafael Nascimento',
-      linkedin: 'a',
+      linkedin: 'https://www.linkedin.com/in/devdagalera/',
       imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
@@ -33,7 +33,7 @@ export default function Team(){
         <div className="max-w-2xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Conheça o nosso time</h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Cococoding é um time de desenvolvedores que tem como objetivo ajudar pessoas a se tornarem desenvolvedores.
+            Cococoding é um time de desenvolvedores que tem como objetivo tirar um 10zão nesse CP!
           </p>
         </div>
         <ul linkedin="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
@@ -43,7 +43,7 @@ export default function Team(){
                 <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
                 <div>
                   <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                  <a href="#" className="text-sm font-semibold leading-6 text-indigo-600">{person.linkedin}</a>
+                  <a target="_blank" href={person.linkedin} className="text-sm font-semibold leading-6 text-indigo-600">Linkedin</a>
                 </div>
               </div>
             </li>
