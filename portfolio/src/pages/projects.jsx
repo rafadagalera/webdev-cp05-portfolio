bfad9edd0d7c3e9dc25a6c77a060511b34ec4163
dagalera <dagalera.dev@gmail.com>
2024-05-02 17:33:45 -0300

    att projeto

--- portfolio/src/pages/projects.jsx
+++ portfolio/src/pages/projects.jsx
@@ -31,7 +31,7 @@ export default function Projects(){
           <div className="">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-10 max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-          <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+          <h2 className="text-2xl font-bold text-gray-900"></h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             {callouts.map((callout) => (
